dca5ff47a9a45c060ce8bc03f561f50e93a5fbcc
Leonel2099 <ezequielleonel1@gmail.com>
2024-03-12 11:46:00 -0300

    Se agrego el terreno creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
+import { Ground } from "./assets/components/Ground";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Sky sunPosition={[100, 100, 100]} />   
         <ambientLight intensity={0.5} />         
         <Physics>
+          <Ground></Ground>
         </Physics>   
       </Canvas>
     </>
