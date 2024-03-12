f024bf9f9038c5790d2714c8fad6cda3d930581a
Leonel2099 <ezequielleonel1@gmail.com>
2024-03-12 11:41:39 -0300

    Se creo el canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-
+import { Canvas } from "@react-three/fiber";
 function App() {
-
   return (
     <>
-     <h1>Hola mundo</h1>
+      <Canvas>
+      </Canvas>
     </>
   )
 }
