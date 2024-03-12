70a95dcad674e1e858cd960c54ea9bc22a5f21c4
Leonel2099 <ezequielleonel1@gmail.com>
2024-03-12 11:43:46 -0300

    Se agrego una lus de ambiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 
+
 function App() {
   return (
     <>
       <Canvas>
-        <Sky sunPosition={[100, 100, 100]} />        
+        <Sky sunPosition={[100, 100, 100]} />   
+        <ambientLight intensity={0.5} />  
       </Canvas>
     </>
   )
