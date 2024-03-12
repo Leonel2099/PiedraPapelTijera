18003b92e23b9baccdb661a1d6bb64ef0c19424c
Leonel2099 <ezequielleonel1@gmail.com>
2024-03-12 11:43:08 -0300

    Se creo un sky

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+
 function App() {
   return (
     <>
       <Canvas>
+        <Sky sunPosition={[100, 100, 100]} />        
       </Canvas>
     </>
   )
